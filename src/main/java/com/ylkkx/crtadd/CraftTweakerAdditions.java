f8fbdd6ab8a31be911f843a1e7c2e1ead993de40
YL-kkx <2324490438@qq.com>
2025-09-26 16:30:45 +0800

    新增关于玩家视角的方法

---
+++ src/main/java/com/ylkkx/crtadd/CraftTweakerAdditions.java
@@ -0,0 +1,7 @@
+package com.ylkkx.crtadd;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION, dependencies = "required-after:crafttweaker")
+public class CraftTweakerAdditions {
+}
---
+++ src/main/java/com/ylkkx/crtadd/utils/CrtAddIPlayer.java
@@ -0,0 +1,56 @@
+package com.ylkkx.crtadd.utils;
+
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import crafttweaker.api.player.IPlayer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import stanhebben.zenscript.annotations.ZenExpansion;
+import stanhebben.zenscript.annotations.ZenMethod;
+import stanhebben.zenscript.annotations.ZenGetter;
+
+@ZenRegister
+@ZenExpansion("crafttweaker.player.IPlayer")
+public class CrtAddIPlayer {
+    //获取玩家视角
+    @ZenGetter("viewType")
+    public static int getViewType(IPlayer player) {
+        // crt类转java类
+        EntityPlayer mcPlayer = CraftTweakerMC.getPlayer(player);
+        // 获取游戏客户端
+        Minecraft mc = Minecraft.getMinecraft();
+
+        // 仅允许操作当前客户端控制的玩家
+        if (mc.player != mcPlayer) {
+            return -1;
+        }
+
+        // 返回原生视角值，与接口中其他getter方法保持简洁风格
+        return mc.gameSettings.thirdPersonView;
+    }
+    //设置玩家的视角
+    @ZenMethod
+    @SideOnly(Side.CLIENT)//客户端
+    public static void togglePlayerView(IPlayer player, int viewType) {
+        // crt类转java类
+        EntityPlayer mcPlayer = CraftTweakerMC.getPlayer(player);
+        // 获取游戏客户端
+        Minecraft mc = Minecraft.getMinecraft();
+
+        // 仅允许操作当前客户端控制的玩家
+        if (mc.player != mcPlayer) {
+            return;
+        }
+
+        // 校验参数合法性
+        if (viewType < 0 || viewType > 2) {
+            throw new IllegalArgumentException("视角类型必须为 0、1 或 2");
+        }
+
+        // 设置视角
+        mc.gameSettings.thirdPersonView = viewType;
+    }
+
+}
